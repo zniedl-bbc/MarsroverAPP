459b76931287802ef6a8ca4fd43226112660e9c7
zguldl-bbc <Lionardo.Guldimann@bbcag.ch>
2016-06-17 11:44:59 +0200

    Start Menu

--- app/src/main/java/bbcag/ch/myapplication/MainActivity.java
+++ app/src/main/java/bbcag/ch/myapplication/MainActivity.java
@@ -1,13 +1,29 @@
 package bbcag.ch.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button exitButton = (Button) findViewById(R.id.button2);
+        exitButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.equals(findViewById(R.id.button2))) {
+            System.exit(0);
+        }
+        if (v.equals(findViewById(R.id.button))) ;
+            //TODO
+
     }
 }
